78747db0158d2b692285ef78e494a731c26bd15a
Jerry Lucas <jerry.lucas@binus.edu>
2023-04-04 12:27:49 +0700

    feat: init project

---
+++ src/test/java/com/employee/express/crudEmployee/CrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.express.crudEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
